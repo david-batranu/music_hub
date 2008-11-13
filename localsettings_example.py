4530ac276f178380b39995dd90831110195563b4
Alex Morega <alex@grep.ro>
2008-11-13 20:34:37 +0200

    moved more local configuration to localsettings.py

---
+++ localsettings_example.py
@@ -0,0 +1,7 @@
+MUSIC_HUB_FOLDER = '/var/local/music_hub/'
+
+ADMINS = (
+    ('MusicHub Admin', 'musichub@example.com'),
+)
+
+SECRET_KEY = '+%(&+3&guiut5=f4$v3b)d@jcu&2wy@(*p((3f#@ya%75sx^-0'
--- settings.py
+++ settings.py
@@ -1,9 +1,4 @@
-# Django settings for music_hub project.
-
-try:
-    from localsettings import MUSIC_HUB_FOLDER
-except ImportError:
-    MUSIC_HUB_FOLDER = '/tmp/'
+from localsettings import MUSIC_HUB_FOLDER, ADMINS, SECRET_KEY
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -21,18 +16,10 @@ LANGUAGE_CODE = 'en-us'
 SITE_ID = 1
 USE_I18N = False
 
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
 MEDIA_ROOT = MUSIC_HUB_FOLDER + 'files/'
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
 MEDIA_URL = '/files/'
 
 ADMIN_MEDIA_PREFIX = '/admin-media/'
-
-SECRET_KEY = '+%(&+3&guiut5=f4$v3b)d@jcu&2wy@(*p((3f#@ya%75sx^-0'
 
 TEMPLATE_LOADERS = (
     'django.template.loaders.app_directories.load_template_source',
